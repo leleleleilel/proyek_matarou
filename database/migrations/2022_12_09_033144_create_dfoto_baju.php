bd7599b32a837ce189bcfcd9f44e2c63de87760f
h0omanOnEarth <clarissagracia.cg@gmail.com>
2022-12-09 22:20:51 +0700

    Update 2022_12_09_033144_create_dfoto_baju.php

--- database/migrations/2022_12_09_033144_create_dfoto_baju.php
+++ database/migrations/2022_12_09_033144_create_dfoto_baju.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        //push again
         Schema::create('d_foto_baju', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_baju');
