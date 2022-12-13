80d91c15d0acdb4d53e5b21406ae27dc4d2e62a4
leleleleilel <markeuuu8@gmail.com>
2022-12-14 00:15:22 +0700

    ui verify email sementara

--- resources/views/verify-email.blade.php
+++ resources/views/verify-email.blade.php
@@ -17,23 +17,30 @@
                     A new email verification link has been emailed to you!
                 </div>
     @endif
-    <div class="jumbotron jumbotron-fluid d-flex justify-content-center" style="margin-top: 50px; width: 50%;">
+    <div class="jumbotron jumbotron-fluid d-flex justify-content-center" style="margin-top: 180px; width: 50%; margin-left: 25%;">
         <div class="container">
             <div class="about-page verifemail">
                 <div class="container text-center">
                     <div class="row">
-                        <div class="col-3">
-                            <img src="{{asset('asset/images/banner/iconverify.png')}}" alt="" style="iconverif">
-                        </div>
-                        <div class="col-9">
-
+                        <div class="col-12">
+                            <img src="{{asset('asset/images/banner/iconverif.png')}}" alt="" style="iconverif">
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="col-4"></div>
+                        <div class="col-3">
+                            <form action="{{ route('verification.send') }}" method="post">
+                            @csrf
+                                <button type="submit" class="btn btn-dark btnreq">Request a new link</button>
+                            </form>
+                        </div>
+                        <div class="col-2">
+                            <a href="{!!url('/home')!!}"><button type="submit" class="btn btn-dark">Home</button></a>
+                        </div>
+                        <div class="col-3"></div>
+                    </div>
                 </div>
-                <form action="{{ route('verification.send') }}" method="post">
-                @csrf
-                    <button type="submit" class="btn btn-dark">Request a new link</button>
-                </form>
+
             </div>
         </div>
     </div>
